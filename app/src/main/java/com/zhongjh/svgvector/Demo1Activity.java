2096c5514281e03f9d7a5c8dbdaed45af4fff82f
zhongjh <254191389@qq.com>
2019-09-23 12:39:45 +0800

    aa

---
+++ app/src/main/java/com/zhongjh/svgvector/Demo1Activity.java
@@ -0,0 +1,16 @@
+package com.zhongjh.svgvector;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Demo1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+    }
+
+}
--- app/src/main/java/com/zhongjh/svgvector/MainActivity.java
+++ app/src/main/java/com/zhongjh/svgvector/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zhongjh.svgvector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 第一个例子
+        findViewById(R.id.btnActivity1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Demo1Activity.class));
+            }
+        });
+
+        // 第二个例子
+        findViewById(R.id.btnActivity2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Demo2Activity.class));
+            }
+        });
+
     }
 }
