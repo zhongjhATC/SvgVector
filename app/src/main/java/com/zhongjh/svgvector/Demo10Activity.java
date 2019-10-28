a5a45c4622da7ea3bea44f690fa98e801f5fb7da
aaatttcccc <254191389@qq.com>
2019-10-29 01:43:21 +0800

    demo10

--- app/src/main/java/com/zhongjh/svgvector/Demo10Activity.java
+++ app/src/main/java/com/zhongjh/svgvector/Demo10Activity.java
@@ -1,7 +1,9 @@
 package com.zhongjh.svgvector;
 
+import android.graphics.drawable.Animatable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Demo10Activity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class Demo10Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo10);
+        ((Animatable) ((ImageView) findViewById(R.id.io16Icon)).getDrawable()).start();
     }
 }
