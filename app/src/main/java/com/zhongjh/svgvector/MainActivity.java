27007784f0091e76a7ce7aa19f35b6395862011a
zhongjh <254191389@qq.com>
2019-11-18 17:54:56 +0800

    demo14

--- app/src/main/java/com/zhongjh/svgvector/MainActivity.java
+++ app/src/main/java/com/zhongjh/svgvector/MainActivity.java
@@ -116,5 +116,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 第十三个例子
+        findViewById(R.id.btnActivity14).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Demo14Activity.class));
+            }
+        });
+
     }
 }
